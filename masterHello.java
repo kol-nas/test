f39e6f533b9d4b2f3a825dcf4822b437d61ff27a
kol-nas <r22106adm@gmail.com>
2022-04-14 23:12:10 +0900

    2nd push

--- masterHello.java
+++ masterHello.java
@@ -1,6 +1,6 @@
 class masterHello{
     public static void main(){
         System.out.primtln("Hello this is master code");
-        System.out.println("identify code is AA00");
+        System.out.println("identify code is AA01");
     }
 }
